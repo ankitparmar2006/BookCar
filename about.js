6b13f8c925431e6669217b0d4363ff757673233a
ankitparmar2006 <ankitparmar30202@gmail.com>
2025-01-07 21:59:49 +0530

    curd work

--- about.js
+++ about.js
@@ -3,24 +3,21 @@
 
 // <!--==================== about hide div  cording start from here  ======================= -->
 
-let aboutbtn=document.querySelector(".about-btn");
+// let aboutbtn=document.querySelector(".about-btn");
+let showoption=()=>
+{
+    let div1=document.querySelector(".about-box9 ");
+    let div2=document.querySelector(".about-box10 ");
+    let div3=document.querySelector(".about-box11 ");
+    let div4=document.querySelector(".about-box12 ");
+    div1.style.display="block";
+    div2.style.display="block";
+    div3.style.display="block";
+    div4.style.display="block";
+  
+}
 
-let div1=document.querySelector(".about-box9 ");
-let div2=document.querySelector(".about-box10 ");
-let div3=document.querySelector(".about-box11 ");
-let div4=document.querySelector(".about-box12 ");
 
-
-aboutbtn.addEventListener("click" , function (){
-    if (div1.style.display==="none" )
-        {
-            div1.style.display="block" 
-        }
-        else {
-            div1.style.display="none" 
-     
-        }
-})
 
 
 
